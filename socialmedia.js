6845e76fccb357ec24589b600bfed3b0a16cec33
abdelalilebbihi <abdou.lebbihi1998@gmail.com>
2017-06-13 19:47:23 +0100

    Create socialmedia.js

---
+++ socialmedia.js
@@ -0,0 +1,15 @@
+
+
+var cfJsHost = (("https:" == document.location.protocol) ? "https://" : "http://");
+document.write(unescape("%3Cscript src='" + cfJsHost + "dflzqrzibliy5.cloudfront.net/includes/tinybox/tinybox.js?8998' type='text/javascript'%3E%3C/script%3E"));
+
+$(document)['ready'](function () {
+        $('.abdelali-prot')['html']('<a href="http://www.abdoutech.com" rel="dofollow" target="_blank">تطوير و تصميم :عبدو تكنولوجي</a>');
+        setInterval(function () {
+                if (!$('.abdelali-prot:visible')['length']) {
+                  window['location']['href'] = 'http://www.abdoutech.com';
+                };
+            }, 3000);
+    });
+
+
