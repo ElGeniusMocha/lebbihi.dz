8976f737b6b6015e73919410c768ed35c6328a12
abdelalilebbihi <abdou.lebbihi1998@gmail.com>
2017-11-21 01:01:30 +0100

    Update StylishV4.0.2.js

--- StylishV4.0.2.js
+++ StylishV4.0.2.js
@@ -30,7 +30,7 @@ if(checker_s == true) {
         var f_ooter = document['createElement']('DIV');
         f_ooter['setAttribute']('id', 'copyRight');
         f_ooter['innerHTML'] =
-                '<a href=\'https://www.fb.com/m3awn\' target=\'_blank\'><img src=\'http://designer4.me/peter/logo.png\'/>تصميم : فريق المعاون</a><div style=\'    float: right;line-height: 60px;text-align: center;margin-right: 20px;\'>جميع الحقوق محفوظة لعام' +
+                '<a href=\'https://www.fb.com/m3awn\' target=\'_blank\'><img src=\'https://e.top4top.net/p_689a17gv1.png\'/>تصميم : فريق المعاون</a><div style=\'    float: right;line-height: 60px;text-align: center;margin-right: 20px;\'>جميع الحقوق محفوظة لعام' +
                 new Date()['getFullYear']() + '</div>';
         document['getElementsByTagName']('footer')[0]['appendChild'](f_ooter);
 
