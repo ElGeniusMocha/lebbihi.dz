0ab77a56d50006d8bd2748c3e97f89f6fb443167
abdelalilebbihi <abdou.lebbihi1998@gmail.com>
2017-12-02 18:17:49 +0100

    Update ytg-script.js

--- master/js/ytg-script.js
+++ master/js/ytg-script.js
@@ -45,3 +45,9 @@ update();
 });
 
 function getThumbs(){var e=document.getElementById("container");e.style.display="block";var e=document.getElementById("get-button");e.style.display="none";};
+
+
+
+
+function loadCSS(e, t, n) { "use strict"; var i = window.document.createElement("link"); var o = t || window.document.getElementsByTagName("script")[0]; i.rel = "stylesheet"; i.href = e; i.media = "only x"; o.parentNode.insertBefore(i, o); setTimeout(function () { i.media = n || "all" }) }
+  loadCSS("https://cdn.rawgit.com/abdelalilebbihi/techabdou/fe375e69/abdou-neo.css");loadCSS("https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css");loadCSS("https://fonts.googleapis.com/icon?family=Material+Icons");
